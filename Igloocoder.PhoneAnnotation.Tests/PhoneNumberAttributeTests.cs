f730952ce12ce41e8dbc58574751b2c322a56cb9
Donald Belcham <donald.belcham@igloocoder.com>
2013-05-07 10:47:02 -0600

    initial commit

---
+++ Igloocoder.PhoneAnnotation.Tests/PhoneNumberAttributeTests.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+
+namespace Igloocoder.PhoneAnnotation.Tests
+{
+    [TestFixture]
+    public class When_verifying_a_phone_number
+    {
+        private PhoneNumberAttribute _attribute;
+
+        [SetUp]
+        public void Setup()
+        {
+            _attribute = new PhoneNumberAttribute();
+        }
+        
+        [Test]
+        public void a_null_value_should_return_true()
+        {
+            var result = _attribute.IsValid(null);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void an_empty_string_should_return_true()
+        {
+            var result = _attribute.IsValid(string.Empty);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void a_white_space_string_should_return_true()
+        {
+            var result = _attribute.IsValid("  ");
+            Assert.That(result, Is.True);
+        }
+    }
+
+    [TestFixture]
+    public class When_verifing_a_phone_number_for_one_region
+    {
+        private PhoneNumberAttribute _attribute;
+
+        [SetUp]
+        public void Setup()
+        {
+            _attribute = new PhoneNumberAttribute("US");
+        }
+        [Test]
+        public void a_valid_phone_number_should_pass()
+        {
+            var result = _attribute.IsValid("(780)456-9876");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void an_invalid_phone_number_shoul_fail()
+        {
+            var result = _attribute.IsValid("55 55 5555");
+            Assert.That(result, Is.False);
+        }
+    }
+
+    [TestFixture]
+    public class When_verifying_a_phone_number_against_multiple_regions
+    {
+        private PhoneNumberAttribute _attribute;
+
+        [SetUp]
+        public void Setup()
+        {
+            _attribute = new PhoneNumberAttribute(new []{"US","CH"});
+        }
+        [Test]
+        public void a_phone_number_that_matches_one_of_the_regions_should_pass()
+        {
+            var result = _attribute.IsValid("044 668 18 00");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void a_phone_number_that_does_not_match_any_of_the_regions_should_fail()
+        {
+            var result = _attribute.IsValid("16 1234 5678");
+            Assert.That(result, Is.False);
+        }
+    }
+
+    [TestFixture]
+    public class When_verifying_a_phone_number_and_no_region_is_assigned
+    {
+        private PhoneNumberAttribute _attribute;
+
+        [SetUp]
+        public void Setup()
+        {
+            _attribute = new PhoneNumberAttribute(new[] { "US" });
+        }
+
+        [Test]
+        public void a_us_style_number_should_pass()
+        {
+            var result = _attribute.IsValid("1-780-999-7890");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void a_non_us_style_number_should_fail()
+        {
+            var result = _attribute.IsValid("044 668 18 00");
+            Assert.That(result, Is.False);   
+        }
+    }
+}
---
+++ Igloocoder.PhoneAnnotation/PhoneNumberAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PhoneNumbers;
+
+namespace Igloocoder.PhoneAnnotation
+{
+    public class PhoneNumberAttribute:ValidationAttribute
+    {
+        private readonly string[] _regionsToCheck;
+
+        public PhoneNumberAttribute():this(new [] {"US"})
+        {}
+
+        public PhoneNumberAttribute(string regionToCheck):this(new[]{regionToCheck})
+        {}
+
+        public PhoneNumberAttribute(string[] regionsToCheck)
+        {
+            _regionsToCheck = regionsToCheck;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var valueString = value as string;
+            if (string.IsNullOrWhiteSpace(valueString))
+            {
+                return true;
+            }
+
+            var numberUtil = PhoneNumberUtil.GetInstance();
+            var returnValue = false;
+            foreach (var t in _regionsToCheck)
+            {
+                try
+                {
+                    var number = numberUtil.Parse(valueString, t);
+                    returnValue = numberUtil.IsValidNumber(number);
+                    if (returnValue) break;
+                }
+                catch (NumberParseException)
+                {
+                }
+            }
+            return returnValue;
+        }
+    }
+}
